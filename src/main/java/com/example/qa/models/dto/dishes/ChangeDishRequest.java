69417ee7c65adeba31144263130a47a01a8ed74d
Aleksandr Li <leealex.2006@mail.ru>
2026-05-11 02:09:05 +0700

    Added more constraints to dto level

--- src/main/java/com/example/qa/models/dto/dishes/ChangeDishRequest.java
+++ src/main/java/com/example/qa/models/dto/dishes/ChangeDishRequest.java
@@ -4,6 +4,8 @@ import com.example.qa.models.enums.DishCategory;
 import com.example.qa.models.enums.Flag;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -26,15 +28,19 @@ public class ChangeDishRequest {
     private List<String> photos;
 
     @JsonProperty(value = "CalorieContent")
+    @DecimalMin("0")
     private Double calorieContent;
 
     @JsonProperty(value = "Proteins")
+    @DecimalMin("0")
     private Double proteins;
 
     @JsonProperty(value = "Fats")
+    @DecimalMin("0")
     private Double fats;
 
     @JsonProperty(value = "Carbohydrates")
+    @DecimalMin("0")
     private Double carbohydrates;
 
     @JsonProperty(value = "Composition")
