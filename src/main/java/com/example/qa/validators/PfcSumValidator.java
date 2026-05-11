a787b85f6c6cdfe0c835c50acc3e82b5fb0828f1
Aleksandr Li <leealex.2006@mail.ru>
2026-05-11 16:14:36 +0700

    Fixed PfcSumValidator

--- src/main/java/com/example/qa/validators/PfcSumValidator.java
+++ src/main/java/com/example/qa/validators/PfcSumValidator.java
@@ -1,20 +1,28 @@
 package com.example.qa.validators;
 
 import com.example.qa.annotations.ValidPfcSum;
+import com.example.qa.models.dto.products.ChangeProductRequest;
 import com.example.qa.models.dto.products.CreateProductRequest;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-public class PfcSumValidator implements ConstraintValidator<ValidPfcSum, CreateProductRequest> {
+public class PfcSumValidator implements ConstraintValidator<ValidPfcSum, Object> {
 
     @Override
-    public boolean isValid(CreateProductRequest dto, ConstraintValidatorContext context) {
-        if (dto == null) return false;
+    public boolean isValid(Object value, ConstraintValidatorContext context) {
+        if (value == null) return true;
 
-        double proteins = dto.getProteins() != null ? dto.getProteins() : 0;
-        double fats = dto.getFats() != null ? dto.getFats() : 0;
-        double carbs = dto.getCarbohydrates() != null ? dto.getCarbohydrates() : 0;
+        double proteins = 100, fats = 100, carbs = 100;
 
+        if (value instanceof CreateProductRequest req) {
+            proteins = req.getProteins() != null ? req.getProteins() : 0;
+            fats = req.getFats() != null ? req.getFats() : 0;
+            carbs = req.getCarbohydrates() != null ? req.getCarbohydrates() : 0;
+        } else if (value instanceof ChangeProductRequest req) {
+            proteins = req.getProteins() != null ? req.getProteins() : 0;
+            fats = req.getFats() != null ? req.getFats() : 0;
+            carbs = req.getCarbohydrates() != null ? req.getCarbohydrates() : 0;
+        }
         return (proteins + fats + carbs) <= 100.0;
     }
 }
